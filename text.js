ad968ba800c13ba03b39cbcfab837b24e8777d61
avinash7avi <aviavinash570@gmail.com>
2023-08-03 18:35:23 +0530

    new problems

---
+++ text.js
@@ -0,0 +1,79 @@
+// var arr = [12,22,23,24]
+// var result=[]
+
+// function odd(arr)
+// {
+//     for(var i =0;i<=arr.lenght;i++)
+//     {
+//         if(arr[i]%2!=0)
+//         result.push(arr[i])   
+        
+//     }
+//     return result
+// }
+// console.log(odd(arr));
+
+
+// (function odd(arr)
+// {
+//  var result =[];
+
+// for(var i =0;i<=arr.lenght;i++){
+
+//     if(arr[i]%2!=0)
+//     {
+//     result.push(arr[i])
+//     }
+    
+// }
+// console.log(result)
+// })([1,2,3,4,5,6,7])
+
+//arrow function=>
+
+// const odd=(arr)=>{
+// var result = []
+//     for(var i=0;i<=arr,length;i++){
+//         if(arr[i]%2!=0){
+//             result.push(arr[i])
+//         }
+//     }
+//     return result
+// }
+// console.log(odd([1,2,3,4,5,6,7])
+
+
+// const person={
+
+//     name: "avinash",
+//     age : 28,
+//     occupation: Worker,
+// }
+// console.log(person.age)
+
+// //square brakets
+// console.log(person["occupation"])
+
+
+
+//map
+//example 1
+// const arr =[1,2,3,4,5,]
+// const output = arr.map((x)=>x*2)
+// console.log(output)
+
+//example 1
+
+// const arr = [1,2,3,4,5,6]
+// const output=arr.map((ele)=>{
+//     return ele*2;
+// })
+// example
+// const arr=[1,2,3,4,5,6]
+// const output=arr.map(function(x){
+//     return x*2;
+// })
+
+
+
+
